d77bbce8cada380b6f8989c3acbdc912c803cca3
Atai Dadush <taibon3535@gmail.com>
2023-09-21 12:05:51 +0300

    feat(client): prepare ground for chess board component

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,14 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="client" />
-    </div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
